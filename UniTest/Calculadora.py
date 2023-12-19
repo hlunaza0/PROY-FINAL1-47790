1a485127dce0097c5d2c0b358b4652cf4f0cb01d
hans <hlunaza@gmail.com>
2023-12-19 00:22:13 -0500

    final

---
+++ UniTest/Calculadora.py
@@ -0,0 +1,20 @@
+def sumar(a,b):
+    return a+b
+
+resultado = sumar(10,9)
+
+if resultado == 19:
+   print("Prueba exitosa")
+else:
+   print("Prueba Fallida")
+
+resultado = sumar(-1,9)
+
+if resultado == 8:
+   print("Prueba existisa")
+else:
+   print("Prueba Fallida")
+
+
+
+    
